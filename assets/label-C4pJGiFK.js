64f7a3fdf4a235d6d94f2f8d65506c32199faa2e
Gonzalo Hernan Coz <GHCoz@farmaplus.com.ar>
2025-12-09 16:26:13 -0300

    Updates

--- assets/label-C4pJGiFK.js
+++
@@ -1,1 +0,0 @@
-import{j as r,ap as a}from"./ui-vendor-lsEhpTbO.js";import{r as t}from"./react-vendor-ylmX0jv_.js";import{f as m,a2 as i}from"./index-DjIolugs.js";const p=i("text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"),l=t.forwardRef(({className:s,...e},o)=>r.jsx(a,{ref:o,className:m(p(),s),...e}));l.displayName=a.displayName;export{l as L};

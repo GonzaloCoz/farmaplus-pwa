8e63d7c69e6cc4bea626a8f5e00e29b57059c34c
Gonzalo Hernan Coz <GHCoz@farmaplus.com.ar>
2025-12-19 10:39:08 -0300

    Updates

---
+++ assets/permissions-BTU5beKC.js
@@ -0,0 +1,6 @@
+import{c as e}from"./index-D3hGVEr9.js";/**
+ * @license lucide-react v0.462.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const r=e("Users",[["path",{d:"M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2",key:"1yyitq"}],["circle",{cx:"9",cy:"7",r:"4",key:"nufk8"}],["path",{d:"M22 21v-2a4 4 0 0 0-3-3.87",key:"kshegd"}],["path",{d:"M16 3.13a4 4 0 0 1 0 7.75",key:"1da9ce"}]]),N={admin:["VIEW_ADMIN_DASHBOARD","MANAGE_INVENTORY_CONFIG","VIEW_BRANCH_MONITOR","EDIT_SETTINGS","IMPERSONATE_BRANCH","EDIT_DASHBOARD_LAYOUT","MANAGE_CALENDAR_EVENTS"],mod:["VIEW_BRANCH_MONITOR","VIEW_ADMIN_DASHBOARD","EDIT_DASHBOARD_LAYOUT","MANAGE_CALENDAR_EVENTS","MANAGE_INVENTORY_CONFIG"],branch:[]};function I(A,E){return A?E==="MANAGE_USERS"?A.username.toLowerCase()==="gcoz":(N[A.role]||[]).includes(E):!1}export{N as R,r as U,I as h};

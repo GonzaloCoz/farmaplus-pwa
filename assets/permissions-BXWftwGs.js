bcca7cba002b7525c8dc676f93eb2e51911b5003
Gonzalo Hernan Coz <GHCoz@farmaplus.com.ar>
2025-12-29 10:44:56 -0300

    Updates

---
+++ assets/permissions-BXWftwGs.js
@@ -0,0 +1,11 @@
+import{c as a}from"./index-BNd3Wt9H.js";/**
+ * @license lucide-react v0.462.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const _=a("Pen",[["path",{d:"M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z",key:"1a8usu"}]]);/**
+ * @license lucide-react v0.462.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const r=a("Users",[["path",{d:"M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2",key:"1yyitq"}],["circle",{cx:"9",cy:"7",r:"4",key:"nufk8"}],["path",{d:"M22 21v-2a4 4 0 0 0-3-3.87",key:"kshegd"}],["path",{d:"M16 3.13a4 4 0 0 1 0 7.75",key:"1da9ce"}]]),s={admin:["VIEW_ADMIN_DASHBOARD","MANAGE_INVENTORY_CONFIG","VIEW_BRANCH_MONITOR","EDIT_SETTINGS","IMPERSONATE_BRANCH","EDIT_DASHBOARD_LAYOUT","MANAGE_CALENDAR_EVENTS"],mod:["VIEW_BRANCH_MONITOR","VIEW_ADMIN_DASHBOARD","EDIT_DASHBOARD_LAYOUT","MANAGE_CALENDAR_EVENTS","MANAGE_INVENTORY_CONFIG"],branch:[]};function I(A,e){return A?e==="MANAGE_USERS"?A.username.toLowerCase()==="gcoz":(s[A.role]||[]).includes(e):!1}export{_ as P,s as R,r as U,I as h};
